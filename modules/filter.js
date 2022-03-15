b2afd15ea31b7ef44e1217ac2e9ae7fb310c0ae5
chevyboys <volcanicchevy@gmail.com>
2022-03-15 00:55:57 +0000

    more robust error handling in filter.js

--- modules/filter.js
+++ modules/filter.js
@@ -344,11 +344,7 @@ async function processCardAction(interaction) {
         .setDescription(embed.description)
         .setTimestamp(flag.createdAt);
 
-        const response = (
-          (infraction.value == 0) ?
-            `The ${member.guild.name} Mods would like to speak with you about the following post. It may be that they're looking for some additional context or just want to handle things informally.\n\n**${mod.displayName}** will be reaching out to you shortly, if they haven't already.` :
-            `We have received one or more complaints regarding content you posted. We have reviewed the content in question and have determined, in our sole discretion, that it is against our code of conduct. This content was removed on your behalf. As a reminder, if we believe that you are frequently in breach of our code of conduct or are otherwise acting inconsistently with the letter or spirit of the code, we may limit, suspend or terminate your access to the ${member.guild.name} Discord server.\n\n**${u.escapeText(mod.displayName)}** has issued this warning.`
-        );
+        const response =  `The ${member.guild.name} Mods would like to speak with you about the following post. It may be that they're looking for some additional context or just want to handle things informally.\n\n**${mod.displayName}** will be reaching out to you shortly, if they haven't already.`;
 
         member.send({ content: response, embeds: [quote] }).catch(() => blocked(member));
       }
